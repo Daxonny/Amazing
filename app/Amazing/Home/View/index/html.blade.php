f305b933917bf9066b1f890c8654b1773c4a3890
Milena199 <stojcevicmilena@gmail.com>
2020-09-21 11:13:12 +0200

    Added artist and tracks

--- app/Amazing/Home/View/index/html.blade.php
+++ app/Amazing/Home/View/index/html.blade.php
@@ -2,15 +2,23 @@
 
 @section('content')
 <section id="stations">
-	<h1>{{ $stations->name }}</h1>
+	<!-- <h1>{{ $stations->name }}</h1> -->
 	<ul class="stationList">
 		@foreach ($stations->stations as $station)
-		<a href="{{ route('player',['station'=>$station->slug]) }}">	
-			<li>
-			<img src="{{ $station->logo }}">
-				<span class="name">{{ str_replace('Amazing', '', $station->name) }}</span>
-			</li>
-		</a>
+		<li id="{{ $station->slug }}" typeof="RadioStation">
+			<h2>
+				<a href="{{ route('player',['station'=>$station->slug]) }}">
+
+					<img src="{{ $station->logo }}">
+					<span class="nameRadio">{{ str_replace('Amazing', '', $station->name) }}</span>
+				</a>
+			</h2>
+			<p class="track" typeof="MusicRecording" data-label="Now Playing">
+				<span class="byArtist">Billy Price & Fred Chapellier</span>
+				<span class="name">My Love Comes Tumbling Down</span>
+			</p>
+			<button class="favourite">star</button>
+		</li>
 		@endforeach
 	</ul>
 </section>
