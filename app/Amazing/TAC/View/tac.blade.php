63b2d128723d9d997fb29658940056c00952b3d0
Milena199 <stojcevicmilena@gmail.com>
2020-10-01 13:34:27 +0200

    Added tac content

--- app/Amazing/TAC/View/tac.blade.php
+++ app/Amazing/TAC/View/tac.blade.php
@@ -11,61 +11,61 @@
 
 <ul class>
 	<li>
-	<p><a href="#section1"> Who we are and how to contact us .</a></p>
+	<p><a href="#section1">Who we are and how to contact us .</a></p>
 	</li>
 	<li>
-	<p>By using our site you accept these terms&nbsp;</p>
+	<p><a href="#section2">By using our site you accept these terms</a></p>
 	</li>
 	<li>
-	<p>There are other terms that may apply to you .</p>
+	<p><a href="#section3">There are other terms that may apply to you . </a></p>
 	</li>
 	<li>
-	<p>We may make changes to these terms .</p>
+	<p><a href="#section4">We may make changes to these terms .</a></p>
 	</li>
 	<li>
-	<p>We may make changes to our site&nbsp;</p>
+	<p><a href="#section5">We may make changes to our site </a></p>
 	</li>
 	<li>
-	<p>We may suspend or withdraw our site .</p>
+	<p><a href="#section6">We may suspend or withdraw our site .</a></p>
 	</li>
 	<li>
-	<p>We may transfer this agreement to someone else .</p>
+	<p><a href="#section7">We may transfer this agreement to someone else . </a></p>
 	</li>
 	<li>
-	<p>You must keep your account details safe&nbsp;</p>
+	<p><a href="#section8">You must keep your account details safe </a></p>
 	</li>
 	<li>
-	<p>How you may use material on our site .</p>
+	<p><a href="#section9">How you may use material on our site . </a></p>
 	</li>
 	<li>
-	<p>Do not rely on information on our site&nbsp;</p>
+	<p><a href="#section10">Do not rely on information on our site </a></p>
 	</li>
 	<li>
-	<p>We are not responsible for websites we link to .</p>
+	<p><a href="#section11">We are not responsible for websites we link to . </a></p>
 	</li>
 	<li>
-	<p>User-generated content is not approved by us .</p>
+	<p><a href="#section12">User-generated content is not approved by us . </a></p>
 	</li>
 	<li>
-	<p>How to complain about content uploaded by other users .</p>
+	<p><a href="#section13">How to complain about content uploaded by other users . </a></p>
 	</li>
 	<li>
-	<p>Our responsibility for loss or damage suffered by you .</p>
+	<p><a href="#section14">Our responsibility for loss or damage suffered by you . </a></p>
 	</li>
 	<li>
-	<p>How we may use your personal information&nbsp;</p>
+	<p><a href="#section15">How we may use your personal information </a></p>
 	</li>
 	<li>
-	<p>We are not responsible for viruses and you must not introduce them&nbsp;</p>
+	<p><a href="#section16">We are not responsible for viruses and you must not introduce them </a></p>
 	</li>
 	<li>
-	<p>Rules about linking to our site&nbsp;</p>
+	<p><a href="#section17">Rules about linking to our site </a></p>
 	</li>
 	<li>
-	<p>Which country&#39;s laws apply to any disputes?&nbsp;</p>
+	<p><a href="#section18">Which country's laws apply to any disputes? </a></p>
 	</li>
 	<li>
-	<p>Our trade marks&nbsp;</p>
+	<p><a href="#section1">Our trade marks </a></p>
 	</li>
 </ul>
 
@@ -77,7 +77,7 @@
 
 <p>To contact us, please email [<strong>Please insert your support email details]</strong>.</p>
 
-<h2>By using our site you accept these terms</h2>
+<h2 id="section2">By using our site you accept these terms</h2>
 
 <p>By using our site, you confirm that you accept these terms of use and that you agree to comply with them.</p>
 
@@ -85,19 +85,19 @@
 
 <p>We recommend that you print a copy of these terms for future reference.</p>
 
-<h2>There are other terms that may apply to you</h2>
+<h2 id="section3">There are other terms that may apply to you</h2>
 
 <p>These terms of use refer to our Privacy Policy <strong>[Please insert as a link to site&rsquo;s privacy policy]</strong>, which also apply to your use of our site.</p>
 
-<h2>We may make changes to these terms</h2>
+<h2 id="section4">We may make changes to these terms</h2>
 
 <p>We amend these terms from time to time. Every time you wish to use our site, please check these terms to ensure you understand the terms that apply at that time.</p>
 
-<h2>We may make changes to our site</h2>
+<h2 id="section5">We may make changes to our site</h2>
 
 <p>We may update and change our site from time to time to reflect changes to our products, our users&#39; needs and our business priorities.</p>
 
-<h2>We may suspend or withdraw our site</h2>
+<h2 id="section6">We may suspend or withdraw our site</h2>
 
 <p>Our site is made available free of charge.</p>
 
@@ -105,11 +105,11 @@
 
 <p>You are also responsible for ensuring that all persons who access our site through your internet connection are aware of these terms of use and other applicable terms and conditions, and that they comply with them.</p>
 
-<h2>We may transfer this agreement to someone else</h2>
+<h2 id="section7">We may transfer this agreement to someone else</h2>
 
 <p>We may transfer our rights and obligations under these terms to another organisation. We will always tell you in writing if this happens and we will ensure that the transfer will not affect your rights under the contract.</p>
 
-<h2>You must keep your account details safe</h2>
+<h2 id="section8">You must keep your account details safe</h2>
 
 <p>If you choose, or you are provided with, a user identification code, password or any other piece of information as part of our security procedures, you must treat such information as confidential. You must not disclose it to any third party.</p>
 
@@ -117,7 +117,7 @@
 
 <p>If you know or suspect that anyone other than you knows your user identification code or password, you must promptly notify us at [<strong>Please insert your support email details]</strong>.</p>
 
-<h2>How you may use material on our site</h2>
+<h2 id="section9">How you may use material on our site</h2>
 
 <p>We are the owner or the licensee of all intellectual property rights in our site, and in the material published on it. Those works are protected by copyright laws and treaties around the world. All such rights are reserved.</p>
 
@@ -131,40 +131,40 @@
 
 <p>If you print off, copy or download any part of our site in breach of these terms of use, your right to use our site will cease immediately and you must, at our option, return or destroy any copies of the materials you have made.</p>
 
-<h2>Do not rely on information on this site</h2>
+<h2 id="section10">Do not rely on information on this site</h2>
 
 <p>The content on our site is provided for general information only. It is not intended to amount to advice on which you should rely. You must obtain professional or specialist advice before taking, or refraining from, any action on the basis of the content on our site.</p>
 
 <p>Although we make reasonable efforts to update the information on our site, we make no representations, warranties or guarantees, whether express or implied, that the content on our site is accurate, complete or up to date.</p>
 
-<h2>We are not responsible for websites we link to</h2>
+<h2 id="section11">We are not responsible for websites we link to</h2>
 
 <p>Where our site contains links to other sites and resources provided by third parties, these links are provided for your information only. Such links should not be interpreted as approval by us of those linked websites or information you may obtain from them.</p>
 
 <p>We have no control over the contents of those sites or resources.</p>
 
-<h2>User-generated content is not approved by us</h2>
+<h2 id="section12">User-generated content is not approved by us</h2>
 
 <p>This website may include information and materials uploaded by other users of the site, including to bulletin boards and chat rooms. This information and these materials have not been verified or approved by us. The views expressed by other users on our site do not represent our views or values.</p>
 
-<h2>How to complain about content uploaded by other users</h2>
+<h2 id="section13">How to complain about content uploaded by other users</h2>
 
 <p>If you wish to complain about content uploaded by other users, please contact us on <strong>[Please insert a hyperlink to contact us section]</strong>.</p>
 
-<h2>Our responsibility for loss or damage suffered by you</h2>
+<h2 id="section14">Our responsibility for loss or damage suffered by you</h2>
 
 <p><strong>If you are a business user</strong>:</p>
 
-<ul class="tac">
+<ul>
 	<li>
 	<p>We exclude all implied conditions, warranties, representations or other terms that may apply to our site or any content on it.</p>
 	</li>
 	<li>
 	<p>We will not be liable to you for any loss or damage, whether in contract, tort (including negligence), breach of statutory duty, or otherwise, even if foreseeable, arising under or in connection with:</p>
 	</li>
-</ul>
 
-<ul class="tac">
+
+<ul>
 	<li>
 	<p>use of, or inability to use, our site; or</p>
 	</li>
@@ -172,12 +172,12 @@
 	<p>use of or reliance on any content displayed on our site.</p>
 	</li>
 </ul>
-
+</ul>
 <ul>
 	<li>
 	<p>In particular, we will not be liable for:</p>
 	</li>
-</ul>
+
 
 <ul>
 	<li>
@@ -196,7 +196,7 @@
 	<p>any indirect or consequential loss or damage.</p>
 	</li>
 </ul>
-
+</ul>
 <p><strong>If you are a consumer user</strong>:</p>
 
 <ul>
@@ -208,11 +208,11 @@
 	</li>
 </ul>
 
-<h2>How we may use your personal information</h2>
+<h2 id="section15">How we may use your personal information</h2>
 
 <p>We will only use your personal information as set out in our Privacy Policy <strong>[Please insert a link to privacy policy]</strong>.</p>
 
-<h2>We are not responsible for viruses and you must not introduce them</h2>
+<h2 id="section16">We are not responsible for viruses and you must not introduce them</h2>
 
 <p>We do not guarantee that our site will be secure or free from bugs or viruses.</p>
 
@@ -220,7 +220,7 @@
 
 <p>You must not misuse our site by knowingly introducing viruses, trojans, worms, logic bombs or other material that is malicious or technologically harmful. You must not attempt to gain unauthorised access to our site, the server on which our site is stored or any server, computer or database connected to our site. You must not attack our site via a denial-of-service attack or a distributed denial-of service attack. By breaching this provision, you would commit a criminal offence. We will report any such breach to the relevant law enforcement authorities and we will co-operate with those authorities by disclosing your identity to them. In the event of such a breach, your right to use our site will cease immediately.</p>
 
-<h2>Rules about linking to our site</h2>
+<h2 id="section17">Rules about linking to our site</h2>
 
 <p>You may link to our home page, provided you do so in a way that is fair and legal and does not damage our reputation or take advantage of it.</p>
 
@@ -234,7 +234,7 @@
 
 <p>If you wish to link to or make any use of content on our site other than that set out above, please contact [<strong>Please insert your support email details]</strong>.</p>
 
-<h2>Which country&#39;s laws apply to any disputes?</h2>
+<h2 id="section18">Which country's laws apply to any disputes?</h2>
 
 <p>If you are a consumer, please note that these terms of use, their subject matter and their formation, are governed by English law. You and we both agree that the courts of England and Wales will have exclusive jurisdiction except that if you are a resident of Northern Ireland you may also bring proceedings in Northern Ireland, and if you are resident of Scotland, you may also bring proceedings in Scotland.</p>
 
