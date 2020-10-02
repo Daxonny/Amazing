c5ef340e041ca84ea8b71b45d6cb01b6219b4d66
Daxonny <50597891+Daxonny@users.noreply.github.com>
2020-10-02 16:05:01 +0200

    CSS player and changed stream links

--- app/Amazing/Player/View/player.blade.php
+++ app/Amazing/Player/View/player.blade.php
@@ -7,17 +7,25 @@
 		<source srcset="{{$station->logo}}.webp, {{$station->logo2x}}.webp 2x" type="image/webp">
 		<img src="{{ $station->logo }}.png" srcset="{{$station->logo}}.png ,{{$station->logo2x}}.png 2x" alt="{{$station->name}}">
 	</picture>
+	<section class="nowPlaying">
+		<p property="track" typeof="MusicRecording" data-label="Now Playing">
+			<span class="Playing">Now Playing</span>
+			<span property="byArtist">Billy Price & Fred Chapellier</span>
+			<span property="name">My Love Comes Tumbling Down</span>
+		</p>
+	</section>
+	<button id="playControl" ln-player-action="play" type="button">play</button>
+	<button id="stopControl" ln-player-action="stop" type="button">stop</button>
 	<div id="volumeControl">
 		<button ln-player-action="mute" type="button">volume_up</button>
 		<input name="range" type="range" min="0" max="100" ln-player-volume="50">
 	</div>
-	<button id="playControl" ln-player-action="play" type="button">play</button>
-	<button id="stopControl" ln-player-action="stop" type="button">stop</button>
 	<audio autoplay>
 		<source src="{{ $station->streams[0]->url }}" />
 	</audio>
-	<!-- <section> 
-			<button>play</button>
-		</section> -->
 </section>
+@endsection
+
+@section('js')
+<script type="text/javascript" src="https://playerservices.live.streamtheworld.com/api/idsync.js?stationId={{ $station->tritonStationId }}"></script>
 @endsection
