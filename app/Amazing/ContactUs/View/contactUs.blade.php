eb6fba732766dab4738d78eabb6a5291818c9fc4
Milena199 <stojcevicmilena@gmail.com>
2020-10-15 15:50:16 +0200

    Added tags

--- app/Amazing/ContactUs/View/contactUs.blade.php
+++ app/Amazing/ContactUs/View/contactUs.blade.php
@@ -1,38 +1,39 @@
 @extends('layout.ln')
 @section('title', 'Contact Us')
 @section('content')
-	<section class="content">
-		<form method="POST" action="{{ url('contactus/send') }}" id="contactForm">
-			@csrf
-			<div id="feName">
-				<label for="name">{{ __('global.name') }}</label>
-				<input type="text" id="name" name="name" placeholder="{{ __('global.name') }}" required>
-				<p>{{ $errors->first('name') }}</p>
-			</div>
+<section class="content">
+	<h1>Contact us</h1>
+	<form method="POST" action="{{ url('contactus/send') }}" id="contactForm">
+		@csrf
+		<div id="feName">
+			<label for="name">{{ __('global.name') }}</label>
+			<input type="text" id="name" name="name" placeholder="{{ __('global.name') }}" required>
+			<p>{{ $errors->first('name') }}</p>
+		</div>
 
-			<div id="feEmail">
-				<label for="email">{{ __('global.email') }}</label>
-				<input type="text" id="email" name="email" placeholder="{{ __('global.email') }}" required>
-				<p> {{ $errors->first('email') }}</p>
-			</div>
+		<div id="feEmail">
+			<label for="email">{{ __('global.email') }}</label>
+			<input type="text" id="email" name="email" placeholder="{{ __('global.email') }}" required>
+			<p> {{ $errors->first('email') }}</p>
+		</div>
 
-			<div id="feMessage">
-				<label for="message">{{ __('global.message') }}</label>
-				<textarea id="text" name="text" placeholder="{{ __('global.writeSomething') }}" required></textarea>
-				<p>{{ $errors->first('text') }}</p>
-			</div>
+		<div id="feMessage">
+			<label for="message">{{ __('global.message') }}</label>
+			<textarea id="text" name="text" placeholder="{{ __('global.writeSomething') }}" required></textarea>
+			<p>{{ $errors->first('text') }}</p>
+		</div>
 
 
-			<input type="submit" value="{{ __('global.submit') }}">
+		<input type="submit" value="{{ __('global.submit') }}">
 
-			<!-- <input type="submit" value="Submit" class="g-recaptcha" 
+		<!-- <input type="submit" value="Submit" class="g-recaptcha" 
 			data-sitekey="reCAPTCHA_site_key" 
 			data-callback='onSubmit' 
 			data-action='submit'> -->
-		  </form>
-	</section>
+	</form>
+</section>
 @endsection
 
 @section('js')
-	<script src="https://www.google.com/recaptcha/api.js"></script>
+<script src="https://www.google.com/recaptcha/api.js"></script>
 @endsection
