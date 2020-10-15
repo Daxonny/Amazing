7763ffeaf25152c6d7e6d2b6f875d800df677d1a
Daxonny <danieldakitodorovski@gmail.com>
2020-10-15 13:08:42 +0200

    fixed header and footer height

--- resources/views/layout/full.blade.php
+++ resources/views/layout/full.blade.php
@@ -168,6 +168,8 @@
 	<script async type="text/javascript" src="{{ asset('/site/v2/js/player/player.js') }}"></script>
 	<script async type="text/javascript" src="{{ asset('/site/v2/js/obfuscator/ln-obfuscator.js') }}"></script>
 
+	<!-- Global site tag (gtag.js) - Google Analytics --> 
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-180609245-1"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-180609245-1'); </script>
 
 	@yield('js') 
 
