6f3c9928a3ad6d894328e53782da7635baf1cf2a
MartinaDimitrievska <dimitrievskamartina666@gmail.com>
2020-11-12 14:08:26 +0100

    fixed preload fonts

--- resources/views/layout/full.blade.php
+++ resources/views/layout/full.blade.php
@@ -12,6 +12,7 @@
 
 	<!-- Preload Fonts -->
 	<link rel="preload" href="{{ lnasset('/fonts/ln-icons/ln-icons.woff2') }}" as="font" type="font/woff2" crossorigin>
+	<link rel="preload" href="https://fonts.gstatic.com/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_ZpC3gnD_g.woff2" as="font" type="font/woff2" crossorigin>
 	<link rel="preload" href="https://fonts.gstatic.com/s/montserrat/v14/JTUSjIg1_i6t8kCHKm459Wlhyw.woff2" as="font" type="font/woff2" crossorigin>
 
 	<!-- CSRF Token -->
